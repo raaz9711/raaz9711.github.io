d8853afbaf9866d0e381c38d3d682c47be7b94ea
raj kumar <raaz.kumar015@gmail.com>
2025-07-23 13:08:23 +0530

    adding initial pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'blog',
+        loadComponent: () => import('./components/blog/blog.component').then(m => m.BlogComponent)
+    },
+    {
+        path: 'contact',
+        loadComponent: () => import('./components/contact/contact.component').then(m => m.ContactComponent)
+    },
+    {
+        path: 'projects',
+        loadComponent: () => import('./components/projects/projects.component').then(m => m.ProjectsComponent)
+    },
+    {
+        path:'about'
+        , loadComponent: () => import('./components/about/about.component').then(m => m.AboutComponent)
+    }
+];
